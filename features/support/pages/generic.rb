d75ffe64b489c261e1cb8546d15ccf887635af1a
Aidan Tallon <aidantallon@hotmail.com>
2017-01-07 17:46:21 +0000

    Now works with localhost

--- features/support/pages/generic.rb
+++ features/support/pages/generic.rb
@@ -5,11 +5,12 @@ class GenericPage
 
   def visit
     @browser.goto @url
+    raise "Not on page" unless on_page?
   end
 
   def on_page?
     begin
-      Watir::Wait.until { @trait.exists? }
+      Watir::Wait.until(timeout: 10) { @trait.exists? }
       return true
     rescue
       return false
